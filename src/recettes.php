5b0554303221a59c0cbc846891f0c2ac86dd2df0
Pitra <justin.pitra@gmail.com>
2024-05-02 14:38:42 +0200

    affichage des recettes

---
+++ src/recettes.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello <?php echo $_POST["username"] ?> </h1>
+
+    <?php
+        // Connect to db
+        $db_connect = new PDO("mysql:host=db;dbname=wordpress", 'root', 'admin');
+        // prepare request
+        $request = $db_connect->prepare("SELECT * FROM recette WHERE author = :username");
+        // bind param (if any)
+        $request->bindParam(":username", $_POST["username"]);
+        // execute request
+        $request->execute();
+        // fetch results
+        $recettes = $request->fetchAll(PDO::FETCH_ASSOC);
+        // show data
+    ?>
+
+    <?php foreach($recettes as $recette):
+        
+        $ingredientsList = explode(";", $recette["ingredients"]);
+        $stepsList = explode(";", $recette["steps"]);
+    ?>
+            
+        <h2><?php echo $recette["name"] ?></h2>
+        <ul>
+            <?php  foreach($ingredientsList as $ingredient):  ?>
+                <li><?php echo $ingredient ?></li>
+            <?php endforeach ?>    
+        </ul>
+        <ul>
+            <?php  foreach($stepsList as $step):  ?>
+                <li><?php echo $step ?></li>
+            <?php endforeach ?>    
+        </ul>
+    <?php endforeach ?>    
+</body>
+</html>
